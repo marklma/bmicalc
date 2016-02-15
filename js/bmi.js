437d0ee430554125fa01444f6b600c8b1b35642b
marklma <marklma@hotmail.com>
2016-02-16 00:02:09 +0800

    calc

--- js/bmi.js
+++ js/bmi.js
@@ -1,1 +1,9 @@
-
+$(function() {
+	$('#submit').click(function(){
+		age = $('#age').val();
+		weight = $('#weight').val();
+		height = $('#height').val();
+		bmi = weight / height / height;
+		$("#bmi").val(bmi);
+	});
+});
